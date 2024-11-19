a58202e0a35bd89c128f8db60e47d5329d795a0d
JRCFull <142862910+JRCFull@users.noreply.github.com>
2024-11-18 22:47:56 -0800

    Update Login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './style.css';
+import React from 'react';
+import './Login.css';
 
 const LoginPage = () => {
-    function getData() {
-        window.location.href = "http://localhost:5000/login";
-    }
+  const handleLogin = () => {
+    // Redirect to the Flask app's home route for Spotify login
+    window.location.href = 'http://localhost:5000/';
+  };
 
-    return (
-        <div className="App">
-            <header className="App-header" style={{ backgroundColor: 'black', color: 'white' }}>
-                <h1 style={{ fontSize: '4rem' }}>Muse</h1>
-
-                <button onClick={getData}>Log in with Spotify</button>
-            </header>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <h1>Muse</h1>
+      <button onClick={handleLogin} className="login-button">
+        Log into Spotify
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
